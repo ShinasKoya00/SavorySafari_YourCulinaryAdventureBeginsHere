e1357467b6501a3c257ed80b1a5e5b87705986a0
Shinas Koya <shinaskoya123@gmail.com>
2024-05-30 23:15:05 +0530

    onboarding done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:savory_safari/screens/homepage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:savory_safari/screens/onboarding_page.dart';
 
