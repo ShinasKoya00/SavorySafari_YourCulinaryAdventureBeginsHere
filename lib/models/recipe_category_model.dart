c6f0f17d4ff67dcfee56123ce58d5043e7cf0441
Shinas Koya <shinaskoya123@gmail.com>
2024-05-31 21:15:15 +0530

    new model updated

---
+++ lib/models/recipe_category_model.dart
@@ -0,0 +1,32 @@
+List recipeCategoryList = [
+  {
+    "heading": "Bread",
+  },
+  {
+    "heading": "Drinks",
+  },
+  {
+    "heading": "Main course",
+  },
+  {
+    "heading": "Salad",
+  },
+  {
+    "heading": "Sandwiches",
+  },
+  {
+    "heading": "Soup",
+  },
+  {
+    "heading": "Sweets",
+  },
+  {
+    "heading": "Desserts",
+  },
+  {
+    "heading": "Drinks",
+  },
+  {
+    "heading": "Drinks",
+  },
+];
